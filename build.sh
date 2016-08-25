d6f564d5f91006cd534f9803e934ad5f7195827d
Łukasz Korecki <lukasz@korecki.me>
2016-08-25 10:41:23 +0100

    Fix sorting

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir -p doc/
 echo "# Notes" > doc/index.md
 
-for f in $(ls *.md  | grep -v README) ; do
+for f in $(ls *.md | sort -r | grep -v README) ; do
   cat $f >> doc/index.md
   echo "> $(date -r $(stat -f '%c' $f))" >> doc/index.md
   echo '---' >> doc/index.md
